8156e960f3ca5ba670bafb5f6c5ca568139d5963
DongWook-Kwak <kdw381@naver.com>
2026-03-09 10:40:17 +0900

    feat : User entity 추가

---
+++ src/main/java/com/zigu/ziguwas/domains/user/entity/User.java
@@ -0,0 +1,43 @@
+package com.zigu.ziguwas.domains.user.entity;
+
+import com.zigu.ziguwas.domains.university.entity.University;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "univ_id", nullable = false)
+    private University univId;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(unique = true , nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column
+    private String profilePhotoUrl;
+
+
+}
