9057c0f324a14b2d483a9a3f0664ef51db522109
DongWook-Kwak <kdw381@naver.com>
2026-03-09 09:56:06 +0900

    initial commit

---
+++ src/main/java/com/zigu/ziguwas/ZiguWasApplication.java
@@ -0,0 +1,13 @@
+package com.zigu.ziguwas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZiguWasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZiguWasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zigu/ziguwas/ZiguWasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zigu.ziguwas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZiguWasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
