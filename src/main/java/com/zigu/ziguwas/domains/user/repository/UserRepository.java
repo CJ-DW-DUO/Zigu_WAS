f3b69539042aa8018c99b653e5fa841dd1a1e4fa
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2026-03-12 10:59:36 +0900

    ref : UserRepository, VerificationStatus 주석추가

--- src/main/java/com/zigu/ziguwas/domains/user/repository/UserRepository.java
+++ src/main/java/com/zigu/ziguwas/domains/user/repository/UserRepository.java
@@ -5,6 +5,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+/**
+ * @author 최정
+ * @since 2026.03.12
+ *
+ * User JPA Repository
+ */
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    /**
+     * 이메일 기반 탐색
+     *
+     * @param email
+     * @return User || null에 대한 예외처리 (orElse 사용필수)
+     */
     Optional<User> findByEmail(String email);
 }
