fdb70b6bb51c188d27f7b7b4fba5327d6d341913
DongWook-Kwak <kdw381@naver.com>
2026-03-14 19:42:12 +0900

    Revert "feat : list 순서변경 방지"
    This reverts commit 0c57eabed601223b7db9f01303901c6bb1675441.

--- src/main/java/com/zigu/ziguwas/security/SecurityConfig.java
+++ src/main/java/com/zigu/ziguwas/security/SecurityConfig.java
@@ -54,7 +54,6 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/email/**").permitAll()
                         .requestMatchers("/api/v1/verify/**").permitAll()
                         .requestMatchers("/api/v1/openai/**").permitAll()
-                        .requestMatchers("/api/v1/items/**").permitAll()
                         .requestMatchers("/swagger-resources/**", "/swagger-ui/**", "/v3/api-docs/**",
                                 "/webjars/**", "/error").permitAll()
                         .requestMatchers("/view/**").permitAll()
