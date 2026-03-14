992cdf4a5b9da47b052989ca23bfa3ef21fc3f91
DongWook-Kwak <kdw381@naver.com>
2026-03-14 17:49:10 +0900

    feat : enumtype 지정

--- src/main/java/com/zigu/ziguwas/domains/user/entity/User.java
+++ src/main/java/com/zigu/ziguwas/domains/user/entity/User.java
@@ -3,6 +3,8 @@ package com.zigu.ziguwas.domains.user.entity;
 import com.zigu.ziguwas.domains.university.entity.University;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -39,6 +41,7 @@ public class User {
     @Column(name = "profile_photo_url")
     private String profilePhotoUrl;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "veri_status")
     private VerificationStatus veriStatus;
 
