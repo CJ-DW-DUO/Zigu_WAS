802074e6b4e2fe29dcb9019b17e2edc72792ce68
DongWook-Kwak <kdw381@naver.com>
2026-03-15 21:11:19 +0900

    feat : 메인이미지를 위한 메소드 추가

--- src/main/java/com/zigu/ziguwas/domains/item/repository/ItemImageRepository.java
+++ src/main/java/com/zigu/ziguwas/domains/item/repository/ItemImageRepository.java
@@ -1,8 +1,16 @@
 package com.zigu.ziguwas.domains.item.repository;
 
+import com.zigu.ziguwas.domains.item.entity.Item;
 import com.zigu.ziguwas.domains.item.entity.ItemImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ItemImageRepository extends JpaRepository<ItemImage, Long> {
+    /**
+     * 특정 아이템의 이미지중 가장 먼저 등록된 하나를 가져옵니다.
+     * @param item item 엔티티
+     */
+    Optional<ItemImage> findFirstByItemOrderByImageIdAsc(Item item);
 
 }
