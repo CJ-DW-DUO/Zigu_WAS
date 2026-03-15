9be45c37233b865d1014867dbad4ba1c8baad9fa
DongWook-Kwak <kdw381@naver.com>
2026-03-15 12:43:09 +0900

    feat : errorcode 추가

--- src/main/java/com/zigu/ziguwas/exception/ErrorCode.java
+++ src/main/java/com/zigu/ziguwas/exception/ErrorCode.java
@@ -12,7 +12,7 @@ public enum ErrorCode {
 
     // 인증 관련
     NOT_CERTIFIED_USER(400, "인증되지 않은 사용자 입니다."),
-
+    UNAUTHORIZED_ACCESS(400, "허용되지 않은 접근입니다." ),
 
     // 기타 관련
     INTERNAL_SERVER_ERROR(500, "내부 서버 오류입니다."),
@@ -20,8 +20,12 @@ public enum ErrorCode {
     // 아이템 관련
     NOT_FOUND_ITEM(404, "아이템을 찾을 수 없습니다." ),
 
+    // 아이템 이미지 관련
+    NOT_FOUND_IMAGE(404, "이미지를 찾을 수 없습니다." ),
+
     // 파일 관련
-    FAIL_UPLOAD_FILE(400, "파일업로드 실패");
+    FAIL_UPLOAD_FILE(400, "파일업로드 실패"),
+    FAIL_DELETE_FILE(400, "파일 삭제 실패" );
 
     private final int status;
     private final String message;
