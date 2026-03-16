327edca746f9c0f0826c980560efba90bf43f03f
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2026-03-16 23:32:20 +0900

    feat : AuthService 로그인 기능

--- src/main/java/com/zigu/ziguwas/security/CustomUserDetailService.java
+++ src/main/java/com/zigu/ziguwas/security/CustomUserDetailService.java
@@ -22,7 +22,7 @@ public class CustomUserDetailService implements UserDetailsService {
         User user = userRepository.findByEmail(email).orElseThrow(
                 () -> new UsernameNotFoundException("이메일로 사용자를 찾을 수 없음"));
         if(user.getVeriStatus() != VerificationStatus.CERTIFIED)
-            throw new CustomException(ErrorCode.NOT_CERTIFIED_USER);
+            throw new CustomException(ErrorCode.USER_NOT_CERTIFIED);
 
         // 이때 반환하는 User는 시큐리티에 있는 User임
         return org.springframework.security.core.userdetails.User.builder()
