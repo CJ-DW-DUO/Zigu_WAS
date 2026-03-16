d70174f07341201c5d4abec6348382cd097e99e7
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2026-03-16 23:31:14 +0900

    feat : 로그인 응답 DTO

---
+++ src/main/java/com/zigu/ziguwas/domains/user/dto/response/LoginResDto.java
@@ -0,0 +1,25 @@
+package com.zigu.ziguwas.domains.user.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class LoginResDto {
+
+    @Schema(example = "1")
+    private Long userId;
+
+    @Schema(example = "zigu@example.com")
+    private String email;
+
+    @Schema(example = "지구헌내기")
+    private String nickname;
+
+    @Schema(example = "618hdjfnvs3jr1f....")
+    private String accessToken;
+
+    @Schema(example = "1fmdivy283yfo1m....")
+    private String refreshToken;
+}
