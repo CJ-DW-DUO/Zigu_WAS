f599b0afce11c3e1893c74b160f13d874d078c8a
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2026-03-16 22:38:29 +0900

    feat : University Repo 이메일 조회 메소드 추가, 주석 추가

--- src/main/java/com/zigu/ziguwas/domains/university/repository/UniversityRepository.java
+++ src/main/java/com/zigu/ziguwas/domains/university/repository/UniversityRepository.java
@@ -3,8 +3,23 @@ package com.zigu.ziguwas.domains.university.repository;
 import com.zigu.ziguwas.domains.university.entity.University;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UniversityRepository extends JpaRepository<University, Long> {
 
+    /**
+     * 해당 도메인을 가진 대학이 존재하는지
+     *
+     * @param univEmail 대학 이메일
+     * @return T/F
+     */
+    boolean existsByUnivEmail(String univEmail);
 
-    boolean existsByUnivEmail(String domain);
+    /**
+     * 해당 도메인을 가진 대학 정보 가져오기
+     *
+     * @param univEmail 대학 이메일
+     * @return 대학 객체
+     */
+    Optional<University> findByUnivEmail(String univEmail);
 }
