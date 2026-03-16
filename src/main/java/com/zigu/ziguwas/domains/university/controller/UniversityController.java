d25f3ab1c7a2cc6d4ca63eef00c37ede02282512
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2026-03-17 02:16:53 +0900

    feat : 대학 검색 API

--- src/main/java/com/zigu/ziguwas/domains/university/controller/UniversityController.java
+++ src/main/java/com/zigu/ziguwas/domains/university/controller/UniversityController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,6 +23,17 @@ public class UniversityController {
     @GetMapping
     public ResponseEntity<?> getAllUniversities(){
         return ResponseEntity.ok().body(universityService.getAllUniversities());
+    }
+
+    /**
+     * 대학 검색 정보 조회 API
+     *
+     * @param name 검색어
+     * @return 검색된 대학 정보 목록
+     */
+    @GetMapping("/search")
+    public ResponseEntity<?> getUniversities(@RequestParam String name){
+        return ResponseEntity.ok().body(universityService.getUniversities(name));
     }
 
 }
