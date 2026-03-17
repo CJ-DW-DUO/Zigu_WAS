271ee128394ffcb1d3a4d070cd1fc13b87e15bf8
DongWook-Kwak <kdw381@naver.com>
2026-03-17 17:36:30 +0900

    refact : 패키지 재분류

--- src/main/java/com/zigu/ziguwas/domains/user/dto/auth/request/EmailVerifyReqDto.java
+++ src/main/java/com/zigu/ziguwas/domains/user/dto/auth/request/EmailVerifyReqDto.java
@@ -1,4 +1,4 @@
-package com.zigu.ziguwas.domains.user.dto.request;
+package com.zigu.ziguwas.domains.user.dto.auth.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
@@ -6,11 +6,13 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
-public class EmailReqDto {
+public class EmailVerifyReqDto {
 
     @Schema(description = "사용자 이메일", example = "zigu@example.com")
     @Email(message = "이메일 형식을 맞춰주세요")
     @NotBlank(message = "이메일은 필수 입력입니다.")
     private String email;
 
+    @Schema(description = "인증 코드", example = "735560")
+    private String code;
 }
