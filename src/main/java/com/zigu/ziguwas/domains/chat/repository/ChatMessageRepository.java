a55933247fd8d638abe524943c161519dde0db32
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2026-03-17 14:16:27 +0900

    fix : 리포지토리 키 String형 오기입 Long으로 수정

--- src/main/java/com/zigu/ziguwas/domains/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/zigu/ziguwas/domains/chat/repository/ChatMessageRepository.java
@@ -3,5 +3,5 @@ package com.zigu.ziguwas.domains.chat.repository;
 import com.zigu.ziguwas.domains.chat.entity.ChatMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChatMessageRepository extends JpaRepository<ChatMessage, String> {
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
 }
--- src/main/java/com/zigu/ziguwas/domains/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/zigu/ziguwas/domains/chat/repository/ChatRoomRepository.java
@@ -3,5 +3,5 @@ package com.zigu.ziguwas.domains.chat.repository;
 import com.zigu.ziguwas.domains.chat.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChatRoomRepository extends JpaRepository<ChatRoom, String> {
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
 }
