44c87f0eff91351416c4c155f5e9f1d0bee5046b
DongWook-Kwak <kdw381@naver.com>
2026-03-19 10:28:05 +0900

    feat : 마이페이지 내가 등록한 item List 조회 API 구현

---
+++ src/main/java/com/zigu/ziguwas/domains/user/dto/mypage/response/MyRegisteredItemResDto.java
@@ -0,0 +1,42 @@
+package com.zigu.ziguwas.domains.user.dto.mypage.response;
+
+import com.zigu.ziguwas.domains.item.entity.Item;
+import com.zigu.ziguwas.domains.item.entity.ItemImage;
+import com.zigu.ziguwas.exception.CustomException;
+import com.zigu.ziguwas.exception.ErrorCode;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+@Builder
+@Schema(description = "나의 등록한 item DTO")
+public class MyRegisteredItemResDto {
+
+    @Schema(description = "아이템Id", example = "1")
+    private final Long itemId;
+
+    @Schema(description = "제목", example = "제목입니다 빌려가세요 상태도 ..")
+    private final String title;
+
+    @Schema(description = "메인이미지 url", example = "사진 url~~~")
+    private final String mainImageUrl;
+
+    @Schema(description = "가격", example = "3000")
+    private final Long dayPerPrice;
+
+    public static MyRegisteredItemResDto fromEntity(Item item) {
+        return MyRegisteredItemResDto.builder()
+                .itemId(item.getId())
+                .title(item.getTitle())
+                .mainImageUrl(item.getImageUrl().stream()
+                        .filter(ItemImage::isMainImageUrl)
+                        .map(ItemImage::getImageUrl)
+                        .findFirst()
+                        .orElseThrow(() -> new CustomException(ErrorCode.IMAGE_NOT_FOUND)))
+                .dayPerPrice(item.getDayPerPrice())
+                .build();
+    }
+}
