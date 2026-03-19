a0ae96d12c4c235bb56609c68fd2d0e3d74b9c50
DongWook-Kwak <kdw381@naver.com>
2026-03-19 10:27:38 +0900

    feat : 유저가 등록한 item list 조회 메소드 추가

--- src/main/java/com/zigu/ziguwas/domains/item/repository/ItemListRepository.java
+++ src/main/java/com/zigu/ziguwas/domains/item/repository/ItemListRepository.java
@@ -1,9 +1,22 @@
 package com.zigu.ziguwas.domains.item.repository;
 
 import com.zigu.ziguwas.domains.item.entity.Item;
+import com.zigu.ziguwas.domains.item.entity.ItemStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
+
 public interface ItemListRepository extends JpaRepository<Item, Long>, JpaSpecificationExecutor<Item> {
+
+    /**
+     * 특정 유저가 등록한 아이템 중 상태가 'REGISTERED'인 것들을 최신순으로 전체 조회합니다.
+     * MyPageService 에서 사용
+     *
+     * @param userId 사용자 식별자
+     * @param status 아이템 상태 (예: ItemStatus.REGISTERED)
+     * @return 해당 조건에 맞는 아이템 리스트
+     */
+    List<Item> findAllByUserIdAndItemStatusOrderByCreatedAtDesc(Long userId, ItemStatus status);
 
 }
