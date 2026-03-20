06d779cca203a1a2cff2d5fe9003424f2b25c979
DongWook-Kwak <kdw381@naver.com>
2026-03-20 11:48:19 +0900

    feat : mypage 대여중인 아이템 리스트 조회 API 구현

---
+++ src/main/java/com/zigu/ziguwas/domains/trade/controller/MyPageTradeController.java
@@ -0,0 +1,35 @@
+package com.zigu.ziguwas.domains.trade.controller;
+
+import com.zigu.ziguwas.domains.trade.api.MyPageTradeApi;
+import com.zigu.ziguwas.domains.trade.dto.response.MyPageTradeListResDto;
+import com.zigu.ziguwas.domains.trade.service.MyPageTradeService;
+import com.zigu.ziguwas.security.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/mypage")
+public class MyPageTradeController implements MyPageTradeApi {
+
+    private final MyPageTradeService myPageTradeService;
+
+    /**
+     * 대여중인 item을 조회합니다.
+     *
+     * @param customUserDetails 현재 로그인한 사용자의 시큐리티 인증 정보
+     * @return 대여중인 item List
+     */
+    @GetMapping("/renting")
+    public ResponseEntity<List<MyPageTradeListResDto>> getRentingItems(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
+
+        List<MyPageTradeListResDto> myPageTradeListResDto = myPageTradeService.findRentingItems(customUserDetails.getUserId());
+        return ResponseEntity.ok(myPageTradeListResDto);
+    }
+}
---
+++ src/main/java/com/zigu/ziguwas/domains/trade/service/MyPageTradeService.java
@@ -0,0 +1,37 @@
+package com.zigu.ziguwas.domains.trade.service;
+
+import com.zigu.ziguwas.domains.trade.dto.response.MyPageTradeListResDto;
+import com.zigu.ziguwas.domains.trade.entity.TradeStatus;
+import com.zigu.ziguwas.domains.trade.repository.MyPageTradeRepository;
+import com.zigu.ziguwas.domains.user.entity.User;
+import com.zigu.ziguwas.domains.user.repository.UserRepository;
+import com.zigu.ziguwas.exception.CustomException;
+import com.zigu.ziguwas.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MyPageTradeService {
+
+    private final MyPageTradeRepository myPageTradeRepository;
+    private final UserRepository userRepository;
+
+    /**
+     * 내가 빌린 물건 중 현재 대여 중인 내역을 조회합니다.
+     *
+     * @param userId 현재 로그인한 사용자의 ID
+     * @return 대여 중인 거래 정보 DTO 리스트
+     */
+    public List<MyPageTradeListResDto> findRentingItems(Long userId) {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+
+        return myPageTradeRepository.findAllByRenteeAndTradeStatus(user, TradeStatus.IN_PROGRESS)
+                .stream().map(MyPageTradeListResDto::fromEntity).toList();
+    }
+
+}
