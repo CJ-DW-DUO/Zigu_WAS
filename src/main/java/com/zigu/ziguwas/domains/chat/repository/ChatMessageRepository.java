04d25da410dedc03b9017a0fa2502342ebe6bb8d
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2026-03-26 13:14:29 +0900

    feat : 채팅방 기준 채팅내용 조회 JPA 메소드

--- src/main/java/com/zigu/ziguwas/domains/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/zigu/ziguwas/domains/chat/repository/ChatMessageRepository.java
@@ -2,6 +2,8 @@ package com.zigu.ziguwas.domains.chat.repository;
 
 import com.zigu.ziguwas.domains.chat.entity.ChatMessage;
 import com.zigu.ziguwas.domains.chat.entity.ChatRoom;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -15,4 +17,14 @@ public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> 
      * @return 가장 최근의 메시지 엔티티
      */
     Optional<ChatMessage> findFirstByChatRoomOrderByTimestampDesc(ChatRoom chatRoom);
+
+
+    /**
+     * 특정 채팅방의 메시지를 페이징하여 조회
+     *
+     * @param chatRoom 채팅방
+     * @param pageable 페이지 번호와 페이지 사이즈
+     * @return 페이지 사이즈에 맞는 채팅 메시지들
+     */
+    Slice<ChatMessage> findByChatRoom(ChatRoom chatRoom, Pageable pageable);
 }
