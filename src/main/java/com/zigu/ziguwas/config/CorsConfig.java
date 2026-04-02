7aa6876f5a1d69c3f805a5624da83b53c52046c5
DongWook-Kwak <kdw381@naver.com>
2026-04-02 10:54:45 +0900

    refact : package 재분류

--- src/main/java/com/zigu/ziguwas/config/CorsConfig.java
+++ src/main/java/com/zigu/ziguwas/config/CorsConfig.java
@@ -1,6 +1,5 @@
-package com.zigu.ziguwas.security;
+package com.zigu.ziguwas.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
