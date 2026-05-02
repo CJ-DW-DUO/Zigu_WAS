3cfec7dfe8598e48d66147b236fb6d6a7b2caa71
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2026-05-02 12:38:29 +0900

    feat : 채팅생성 DTO 구조 변경

---
+++ src/main/java/com/zigu/ziguwas/domains/chat/dto/request/CreateChatRoomReqDto.java
@@ -0,0 +1,26 @@
+package com.zigu.ziguwas.domains.chat.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateChatRoomReqDto {
+
+    @Schema(description = "채팅 대상자 ID", example = "2")
+    @NotBlank(message = "채팅 대상자 ID는 입력해야 합니다.")
+    @Positive(message = "채팅 대상자 ID는 양수여야 합니다.")
+    private Long receiverId;
+
+    @Schema(description = "물품 ID", example = "1")
+    @NotBlank(message = "물품 ID는 입력해야 합니다.")
+    @Positive(message = "물품 ID는 양수여야 합니다.")
+    private Long ItemId;
+}
