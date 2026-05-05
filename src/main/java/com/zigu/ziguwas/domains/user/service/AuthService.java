7b3016a6af5f72969b7f96475868ee74f611ed58
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2026-05-05 09:33:56 +0900

    fix : 이메일 인증 만료시간 단위 불일치 수정2
    밀리초라 100이 아닌 1000을 곱해야하네요

--- src/main/java/com/zigu/ziguwas/domains/user/service/AuthService.java
+++ src/main/java/com/zigu/ziguwas/domains/user/service/AuthService.java
@@ -91,7 +91,7 @@ public class AuthService {
         String verificationCode = String.valueOf((int)(Math.random() * 899999) + 100000);
 
         // Redis에 저장 / 이메일, 인증코드, 5분
-        redisService.setDataExpire(dto.getEmail(), verificationCode, 300 * 100);
+        redisService.setDataExpire(dto.getEmail(), verificationCode, 300 * 1000);
 
         // 3. 이메일 발송
         SimpleMailMessage message = new SimpleMailMessage();
@@ -123,7 +123,7 @@ public class AuthService {
             // 인증 코드는 삭제
             redisService.deleteData(dto.getEmail());
             // 인증된 메일 상태는 10분간 유지하도록
-            redisService.setDataExpire(dto.getEmail(), "DONE", 600 * 100);
+            redisService.setDataExpire(dto.getEmail(), "DONE", 600 * 1000);
         } else {
             // 인증 정보가 다를경우 예외
             throw new CustomException(ErrorCode.VERIFY_CODE_NOT_MATCHED);
