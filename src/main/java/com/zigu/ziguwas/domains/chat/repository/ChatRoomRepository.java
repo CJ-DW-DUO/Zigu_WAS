4ec2ae53df94132091ea5eb1b63f576c388d7c27
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2026-06-27 15:35:04 +0000

    feat : 채팅방 ID 기반 조회 메소드 추가

--- src/main/java/com/zigu/ziguwas/domains/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/zigu/ziguwas/domains/chat/repository/ChatRoomRepository.java
@@ -3,5 +3,9 @@ package com.zigu.ziguwas.domains.chat.repository;
 import com.zigu.ziguwas.domains.chat.entity.ChatRoom;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface ChatRoomRepository extends MongoRepository<ChatRoom, String> {
+
+    Optional<ChatRoom> findById(String id);
 }
