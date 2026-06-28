e489c73bfb438385cfdd6b5d09b24b6ad46bb51c
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2026-06-28 11:33:48 +0900

    feat : Notification 엔티티 MongoDB Document로 전환 및 인덱스 설계

--- src/main/java/com/zigu/ziguwas/domains/notification/entity/Notification.java
+++ src/main/java/com/zigu/ziguwas/domains/notification/entity/Notification.java
@@ -1,23 +1,30 @@
 package com.zigu.ziguwas.domains.notification.entity;
 
-import com.zigu.ziguwas.domains.user.entity.User;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.CompoundIndexes;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
-@Entity
+@Document(collection = "notifications")
+/**
+ * name : 인덱스 이름 (Mongo에서 확인할 때 보이는 이름, 식별하기 쉽게만 지으면 됨)
+ * def : 어떤 필드를 인덱스로 묶을지 정의, 1은 오름차순, -1은 내림차순
+ */
+@CompoundIndexes({
+        // 알림목록 조회 : userId와 recTime을 묶어서 인덱스를 생성, recTime은 내림차순으로 정렬
+        // userId가 123인 알림을 recTime 최신순(내림차순)으로 가져오기
+        @CompoundIndex(name = "idx_userId_recTime", def = "{'userId': 1, 'recTime': -1}"),
+        // 미읽음 카운트 : userId와 isRead를 묶어서 인덱스를 생성, isRead는 오름차순으로 정렬
+        // userId가 123인 읽지않은(isRead가 false)인 알림이 몇개인지 카운트하기 위해 isRead를 오름차순으로 정렬(이때 오름차순 정렬은 의미없음)
+        @CompoundIndex(name = "idx_userId_isRead",  def = "{'userId': 1, 'isRead': 1}")
+})
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -25,31 +32,22 @@ import java.time.LocalDateTime;
 public class Notification {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "noti_id")
-    private Long notiId;
+    private String id;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+    private Long userId;
 
-    @Enumerated(EnumType.STRING)
-    @Column(name = "noti_type", nullable = false)
     private NotificationType notificationType;
 
-    @Column(name = "noti_title", nullable = false)
     private String notiTitle;
 
-    @Column(name = "noti_content", nullable = false)
     private String notiContent;
 
-    @Column(name = "rec_time", nullable = false)
+    // ISO-8601 형식, 90일, 오래된 알림은 제거하기 위한 TTL 인덱스 설정
+    @Indexed(expireAfter = "P90D")
     private LocalDateTime recTime;
 
-    @Column(name = "is_read", nullable = false)
     private Boolean isRead;
 
-    @Column(name = "read_at")
     private LocalDateTime readAt;
 
     /**
@@ -59,13 +57,13 @@ public class Notification {
      * 읽음 상태는 기본값(false)으로 초기화합니다.
      */
     public static Notification create(
-            User user,
+            Long userId,
             NotificationType notificationType,
             String notiTitle,
             String notiContent
     ) {
         return Notification.builder()
-                .user(user)
+                .userId(userId)
                 .notificationType(notificationType)
                 .notiTitle(notiTitle)
                 .notiContent(notiContent)
